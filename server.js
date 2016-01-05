33465dd49454b7b4a65cf0ca9798448df0b57566
Tyler Hedegard <tchedegard@gmail.com>
2016-01-05 06:18:27 +0000

    added async for top tracks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
+var async = require('async');
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
@@ -26,6 +27,8 @@ app.get('/search/:name', function(req, res) {
         limit: 20,
         type: 'artist'
     });
+    
+    var errorCheck = false;
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
@@ -33,16 +36,86 @@ app.get('/search/:name', function(req, res) {
         
         relatedArtists.on('end', function(item) {
             artist.related = item.artists;
-            res.json(artist);
+            
+            var countArtists = 0;
+            
+            var checkComplete = function() {
+                if (countArtists === artist.related.length) {
+                    res.json(artist);
+                }
+                console.log('Check Complete hit');
+                console.log('countArtists = '+countArtists);
+                console.log('artist.related.length = '+artist.related.length);
+            };
+            
+            //This was the attempt without using async module
+            //I couldn't find a way to match the async reply with the right index position of artist.related
+            
+            /*for (var i in artist.related) {
+                var topSongs = getFromApi('artists/'+artist.related[i].id+'/top-tracks?country=US');
+                
+                topSongs.on('end', function(item) {
+                    artist.related[countArtists].tracks = item.tracks;
+                    countArtists += 1;
+                    checkComplete();
+                });
+                
+                topSongs.on('error', function(code) {
+                    if (artist.related.length === false) {
+                        res.sendStatus(code);
+                        artist.related.length = true;
+                    }
+                });
+            }*/
+            
+            //Here is the start of using async module for top-tracks
+            var idArray = [];
+            
+            for (var i in artist.related) {
+                idArray.push(artist.related[i].id);
+            }
+            
+            async.each(idArray,
+                function(id) {
+                    var topSongs = getFromApi('artists/'+id+'/top-tracks?country=US');
+                    
+                    topSongs.on('end', function(item) {
+                        for (var j in artist.related) {
+                            if (id === artist.related[j].id) {
+                                artist.related[j].tracks = item.tracks;
+                            }
+                        }
+                        countArtists += 1;
+                        if (countArtists === artist.related.length) {
+                            res.json(artist);
+                            console.log('HIT');
+                        }
+                    });
+                    
+                    topSongs.on('error', function(code) {
+                        if (errorCheck === false) {
+                            res.sendStatus(code);
+                            errorCheck = true;
+                        }
+                    });
+                }, 
+                checkComplete() //Can't explain why this runs before all calls are done.
+            );
         });
         
         relatedArtists.on('error', function(code) {
-            res.sendStatus(code);
+            if (errorCheck === false) {
+                res.sendStatus(code);
+                errorCheck = true;
+            }
         });
     });
 
     searchReq.on('error', function(code) {
-        res.sendStatus(code);
+        if (errorCheck === false) {
+            res.sendStatus(code);
+            errorCheck = true;
+        }
     });
 });
 
